1e127f78e6cc400e1dd8dfc5158fcda6a6a6cde4
yunis <yhuseynov19@gmail.com>
2020-11-19 14:13:29 +0300

    settings

--- ezuo/settings.py
+++ ezuo/settings.py
@@ -13,19 +13,17 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+#ry@y*1u1sm^zdv(fw17m#!2!)j(qn4y@q7@7*!sg7gd8+h2a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -84,12 +82,7 @@ WSGI_APPLICATION = 'ezuo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
@@ -132,9 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/img/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
 """
@@ -240,3 +230,9 @@ EMAIL_HOST_PASSWORD = 'ezuo1992local'
 EMAIL_PORT = 465  
 
 #mail
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    from .prod_settings import *
